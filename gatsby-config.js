cb41d4b5a08bfb64d5e8d3d51861edf569e65393
daviddprtma <davidchristianpratama@gmail.com>
2022-07-04 11:13:21 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
